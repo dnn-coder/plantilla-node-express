ea285844fed2300ed5e64279c3e1b602e415026e
Danny Vivas <dvivasmera@gmail.com>
2025-11-20 09:24:23 -0500

    rate limit, numero de peticiones, bloqueo del servidor

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+//licrerias
 const express = require("express")
+const rateLimit = require("express-rate-limit")
+
+//Routers
 const { usersRouter } = require("./routes/users.routes")
 const { postsRouter } = require("./routes/posts.routes")
-const { globalErrorHandler } = require("./controllers/error.controller")
-const { AppError } = require("./utils/appError.util")
 const { commentRouter } = require("./routes/comments.routes")
+
+//manejador global de errores
+const { globalErrorHandler } = require("./controllers/error.controller")
+
+//Utils
+const { AppError } = require("./utils/appError.util")
+
+
 
 const app = express()
 
 app.use(express.json())
+
+const limiter = rateLimit({
+	max: 5,
+	windowMs: 60 * 1000, //1min
+	message: 'has excedido el numero de solicitudes permitidas'
+})
+
+app.use(limiter)
 
 app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/posts', postsRouter)
